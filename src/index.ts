48d55658dbe3701d076f6b0ba597c8b30a0cc9c4
ntombizodwantlahla53 <ntombizodwantlahla53@gmail.com>
2026-09-11 13:54:47 +0200

    added promise

--- src/index.ts
+++ src/index.ts
@@ -47,54 +47,111 @@
 
 // console.log("doing another task")
 
-function fetchUserId(callback:(error: Error | null, userId?: string) =>void){
-    console.log("fetching userId")
-    setTimeout(() =>{
-        const userId = "user 123"
-        callback(null, userId)
-    },2000)
-}
 
-function fetchUserDetails(userId:string ,callback: (error: Error | null, details?: {name:string; email:string}) =>void){
-    console.log(`fetching details for userId:, ${userId}`);
-    setTimeout(() =>{
-        const details ={name:"zzz" , email:"adas@gmail.com"}
-        callback(null ,details)
-    },3000)
-}
 
-function saveUserLog(userName: string, userEmail: string, callback: (error: Error | null, logStatus?: string) =>void){
-    console.log(`saving log for ${userName} with ${userEmail}`);
-    setTimeout(() =>{
-        const status = "log saved successfully"
-        callback(null, status)
-    },3000)
-}
 
-fetchUserId((error, userId) =>{
-    if(error){
-        console.error("error fetching user Id", error.message)
-        return;
-    }
-    if (userId){
-        fetchUserDetails(userId, (error, details) =>{
-            if(error){
-                console.log("error fetching user details", error.message)
-                 return;
-            }
+
+
+
+
+// function fetchUserId(callback:(error: Error | null, userId?: string) =>void){
+//     console.log("fetching userId")
+//     setTimeout(() =>{
+//         const userId = "user 123"
+//         callback(null, userId)
+//     },2000)
+// }
+
+// function fetchUserDetails(userId:string ,callback: (error: Error | null, details?: {name:string; email:string}) =>void){
+//     console.log(`fetching details for userId:, ${userId}`);
+//     setTimeout(() =>{
+//         const details ={name:"zzz" , email:"adas@gmail.com"}
+//         callback(null ,details)
+//     },3000)
+// }
+
+// function saveUserLog(userName: string, userEmail: string, callback: (error: Error | null, logStatus?: string) =>void){
+//     console.log(`saving log for ${userName} with ${userEmail}`);
+//     setTimeout(() =>{
+//         const status = "log saved successfully"
+//         callback(null, status)
+//     },3000)
+// }
+
+// fetchUserId((error, userId) =>{
+//     if(error){
+//         console.error("error fetching user Id", error.message)
+//         return;
+//     }
+//     if (userId){
+//         fetchUserDetails(userId, (error, details) =>{
+//             if(error){
+//                 console.log("error fetching user details", error.message)
+//                  return;
+//             }
         
-    if (details){
-        saveUserLog(details.name, details.email, (error, logStatus) =>{
-            if(error){
-                console.log("error is saving user details", error.message)
-                 return;
-            }
-            if(logStatus){
-                console.log("all operations completed successfully")
-                console.log("final status:", logStatus)
-            }
-            })
-            }
-        })
-    }
+//     if (details){
+//         saveUserLog(details.name, details.email, (error, logStatus) =>{
+//             if(error){
+//                 console.log("error is saving user details", error.message)
+//                  return;
+//             }
+//             if(logStatus){
+//                 console.log("all operations completed successfully")
+//                 console.log("final status:", logStatus)
+//             }
+//             })
+//             }
+//         })
+//     }
+// })
+
+
+
+function promiseFetchUserId(): Promise<string>{
+    console.log("fetching user id")
+    return new Promise((resolve,reject) =>{
+        setTimeout(() =>{
+            const userId = "user123";
+            // if(err){
+            //     return reject(new Error("failed top fetch user id"))
+            // }
+            resolve(userId);
+        },1000)
+    })
+}
+
+function promiseFetchUserDetails(userId: string): Promise<{name: string; email:string}>{
+console.log(`fetching details for userId: ${userId}`);
+ return new Promise((resolve,reject) => {
+    setTimeout(() =>{
+        const details = {name: "propropro", email: "promise@gmail.com"}
+        resolve(details)
+    },2000)
+ })
+}
+
+function promiseSaveUserLog(userName: string, userEmail:string): Promise<string>{
+    console.log(`Saving user log for ${userName} ,email: ${userEmail}`);
+    return new Promise((resolve,reject) =>{
+        setTimeout(() =>{
+            const status = "Log Saved succesfully"
+            resolve(status)
+        },2000)
+    })
+}
+
+promiseFetchUserId().then((userId) => {
+    return promiseFetchUserDetails(userId)
+}).then((details) => {
+    return promiseSaveUserLog(details.name, details.email)
+}).then((logStatus) =>{
+    console.log("all operations completed successfully")
+    console.log("final status:", logStatus)
+}).catch((error) =>{
+    console.error("An error occured in the promise chain:", error.message)
 })
+
+async function name(params: type){
+    await 
+}
